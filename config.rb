2b9194f9a21c1c6363031c42507579560a3d647b
Dima Koval <dkoval@heliostech.fr>
2018-08-30 13:50:38 +0300

    Render docs as html

--- config.rb
+++ config.rb
@@ -5,6 +5,8 @@ activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
+page '/docs/*', layout: 'layout', content_type: 'text/html'
+
 # Layouts
 # https://middlemanapp.com/basics/layouts/
 
